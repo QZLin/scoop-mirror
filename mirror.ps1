fc8c56e081f442d54ec3c0469e05579a757ca75d
QZLin <qzlin01@163.com>
2024-09-20 10:46:11 +0800

    update for scoop dev

--- mirror.ps1
+++ mirror.ps1
@@ -1,14 +1,13 @@
 $simple_replace = @{
-    # 'github\.com'                          = 'hub.fastgit.org'
-    # 'raw\.githubusercontent\.com'          = 'raw.fastgit.org'
-    'https?://github\.com'                 = 'https://ghproxy.net/github.com'
-    'https?://raw\.githubusercontent\.com' = 'https://ghproxy.net/raw.githubusercontent.com'
-    'https?://raw\.github\.com'            = 'https://ghproxy.net/raw.github.com'
-    #    '(?<=//)(?:.*dl|downloads)\.sourceforge\.net' = 'udomain.dl.sourceforge.net'
+    'https?://github\.com'                        = 'https://ghproxy.net/github.com'
+    'https?://raw\.githubusercontent\.com'        = 'https://ghproxy.net/raw.githubusercontent.com'
+    'https?://raw\.github\.com'                   = 'https://ghproxy.net/raw.github.com'
+    "https?://nodejs\.org/dist/"                  = 'https://registry.npmmirror.com/-/binary/node/'
+    '(?<=//)(?:.*dl|downloads)\.sourceforge\.net' = 'nchc.dl.sourceforge.net'
 }
 function inject_url ($url) {
-    foreach ($_ in $simple_replace.Keys) {
-        $url = $url -replace $_, $simple_replace.$_
+    foreach ($key in $simple_replace.Keys) {
+        $url = $url -replace $key, $simple_replace.$key
     }
     info $url
     return $url
--- run_inject.ps1
+++ run_inject.ps1
@@ -1,22 +1,50 @@
-param([switch]$link = $false)
-$scoop_home = Get-Item $env:USERPROFILE"/scoop/apps/scoop/current"
+param(
+    [switch]$link = $false, 
+    [switch]$dev = $false,
+    [Parameter(Mandatory = $false)]
+    $scoop_home = $null
+)
+if (-not $scoop_home) {
+    $scoop_home = Get-Item $env:USERPROFILE"/scoop/apps/scoop/current"
+}
 Push-Location $scoop_home
 
+# mirror
+$lib_mirror = "lib/mirror.ps1"
 if ($link) {
-    if (-not(Test-Path "lib/mirror.ps1")) {
-        New-Item -ItemType SymbolicLink -Path "lib/mirror.ps1" -Value (Get-Item $PSScriptRoot/mirror.ps1) 
+    if (Test-Path $lib_mirror) {
+        Remove-Item -Path $lib_mirror
+    }
+    New-Item -ItemType SymbolicLink -Path $lib_mirror -Value (Get-Item "$PSScriptRoot/mirror.ps1") 
+}
+else {
+    Copy-Item "$PSScriptRoot/mirror.ps1" $lib_mirror -Force
+}
+
+
+# inject
+$patern = '(?<!inject_url\()handle_special_urls \$url', 'inject_url(handle_special_urls $url)'
+
+if ($dev) {
+    $lib_download = "lib/download.ps1"
+    $lib_download_ps1 = Get-Content $lib_download
+    $result = $lib_download_ps1 -replace '(?<!inject_url\()handle_special_urls \$url', 'inject_url(handle_special_urls $url)'
+    Set-Content -Path $lib_download -Value $result
+
+    if (-not $lib_download_ps1.Contains(". `$PSScriptRoot/mirror.ps1")) {
+        Write-Output ('. $PSScriptRoot/mirror.ps1') >> $lib_download
     }
 }
 else {
-    Copy-Item $PSScriptRoot/mirror.ps1 "lib/mirror.ps1" -Force
+    $lib_install = "lib/install.ps1"
+    $lib_install_ps1 = Get-Content $lib_install
+    $result = $lib_install_ps1 -replace $patern
+    Set-Content -Path $lib_install -Value $result
+
+    if (-not $lib_install_ps1.Contains(". `$PSScriptRoot/mirror.ps1")) {
+        Write-Output (". `$PSScriptRoot/mirror.ps1") >> $lib_install
+    }
 }
 
-$lib_install_ps1 = Get-Content lib/install.ps1
-$result = $lib_install_ps1 -replace `
-    "(?<!inject_url\()handle_special_urls \`$url", "inject_url(handle_special_urls `$url)"
-Set-Content -Path lib/install.ps1 -Value $result
-if (-not $lib_install_ps1.Contains(". `$PSScriptRoot/mirror.ps1")) {
-    Write-Output (". `$PSScriptRoot/mirror.ps1") >> "lib/install.ps1"
-}
 Write-Output "Finished."
 Pop-Location
